0fccf974f84aacf744f725d89eb21ae8b85a2305
Jasmine-Ball <jasmine.ball@uon.edu.au>
2016-07-28 08:23:03 +1000

    Add basic code

--- PowerShell/ping-report.ps1
+++ PowerShell/ping-report.ps1
@@ -1,1 +1,5 @@
-# Ping report
+$hostname = Get-Content -Path list.txt
+$ping = ping $hostname | Select-String "Packets"
+Write-Host $hostname[*] $ping
+
+# need to modify this to get the array loop working, and add the array index to both $hostname variables.
