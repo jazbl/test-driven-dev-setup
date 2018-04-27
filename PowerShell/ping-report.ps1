16ac629eccaea18345851ae4f6c1253e1b1531c2
Jasmine-Ball <jasmine.ball@uon.edu.au>
2018-04-28 02:20:29 +1000

    Moved to System-Administration-scripts

--- PowerShell/ping-report.ps1
+++
@@ -1,7 +0,0 @@
-# Create an integer $counter loop
-$looper = $counter
-$hostname = Get-Content -Path list.txt
-$ping = ping $hostname[$looper] | Select-String "Packets"
-Write-Host $hostname[$looper] $ping
-
-# need to modify this to get the array loop working, and add the array index to both $hostname variables.
